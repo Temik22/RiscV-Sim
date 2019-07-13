184cc4a3a1cbb200ea221bdf45116e3f718711e2
Temik22 <strokov-artem@mail.ru>
2019-07-13 16:31:06 +0400

    added simulator that perform some instructions and think about debug mode

--- riscsim.py
+++ riscsim.py
@@ -4,6 +4,25 @@ import argparse
 # todo list:
 # 1. how to organize memory and have classification between .word/.half/.byte
 # 2. organize debug mode
+# 3. make less params
+
+altRegNames = {'zero': 'x0', 'ra': 'x1', 'sp': 'x2', 'gp': 'x3', 'tp': 'x4', 't0': 'x5', 't1': 'x6', 't2': 'x7', 's0': 'x8', 's1': 'x9', 'a0': 'x10', 'a1': 'x11',
+               'a2': 'x12', 'a3': 'x13', 'a4': 'x14', 'a5': 'x15', 'a6': 'x16', 'a7': 'x17', 's2': 'x18', 's3': 'x19', 's4': 'x20', 's5': 'x21', 's6': 'x22', 's7': 'x23',
+               's8': 'x24', 's9': 'x25', 's10': 'x26', 's11': 'x27', 't3': 'x28', 't4': 'x29', 't5': 'x30', 't6': 'x31'}
+
+
+class machineAnswer:
+    name = ''
+    status = ''
+
+
+class startStatus:
+    debug = False
+    startLabel = ''
+
+    def __init__(self, debug, label):
+        self.debug = debug
+        self.startLabel = label
 
 
 class answerFromMem:
@@ -15,7 +34,7 @@ class answerFromMem:
         self.startValue = value
 
 
-class MemoryBit:
+class MemoryByte:
     value = 0
     readonly = False
 
@@ -123,7 +142,6 @@ def parseDirectives(machine, file):
                 if file.directives[j].startswith('.'):
                     break
                 settings.append(memoryAdd(machine, file.directives[j], True))
-    print(settings)
 
 
 def memoryAdd(machine, string, type):
@@ -134,12 +152,118 @@ def memoryAdd(machine, string, type):
     return temp[0]
 
 
-def makeInstuctions(machine, marks, instructions, startLabel):
-    # TODO()
+def performInstructions(machine, marks, instructions, startStatus):
+    # Think about end point that stops simulation
     end = False
-    current = marks.get(startLabel)
+    current = marks.get(startStatus.startLabel)
     while not end:
         line = instructions[current]
+        command = line.split(' ')[0]
+        line = line[len(command):].split(',')
+        line = [i.strip() for i in line]
+        print('{0} : {1}'.format(command, line))
+        if command == 'lw':
+            setRegValue(machine, line[0], int(line[1]))
+
+        elif command == 'add':
+            result = getRegValue(
+                machine, line[1]) + getRegValue(machine, line[2])
+            setRegValue(machine, line[0], result)
+
+        # I think it must be rewrited with find()
+        # add and addi have same function but different arguments
+        #
+        # elif command == 'addi':
+        #     result = getRegValue(machine, line[1]) + int(line[2])
+        #     setRegValue(machine, line[0], result)
+
+        elif command == 'sub':
+            result = getRegValue(
+                machine, line[1]) - getRegValue(machine, line[2])
+            setRegValue(machine, line[0], result)
+
+        elif command == 'and':
+            result = getRegValue(
+                machine, line[1]) & getRegValue(machine, line[2])
+            setRegValue(machine, line[0], result)
+
+        elif command == 'or':
+            result = getRegValue(
+                machine, line[1]) | getRegValue(machine, line[2])
+            setRegValue(machine, line[0], result)
+
+        elif command == 'xor':
+            result = getRegValue(
+                machine, line[1]) ^ getRegValue(machine, line[2])
+            setRegValue(machine, line[0], result)
+
+        elif command == 'sll':
+            result = getRegValue(
+                machine, line[1]) << getRegValue(machine, line[2])
+            setRegValue(machine, line[0], result)
+
+        elif command == 'srl':
+            result = getRegValue(
+                machine, line[1]) >> getRegValue(machine, line[2])
+            setRegValue(machine, line[0], result)
+
+        elif command == 'j':
+            if marks.get(line[0]) != None:
+                current = marks[line[0]] - 1
+            else:
+                print('jump error')
+
+        elif command == 'beq':
+            if getRegValue(machine, line[0]) == getRegValue(machine, line[1]):
+                current = marks[line[2]] - 1
+            else:
+                continue
+
+        elif command == 'bne':
+            if getRegValue(machine, line[0]) != getRegValue(machine, line[1]):
+                current = marks[line[2]] - 1
+            else:
+                continue
+
+        elif command == 'bge':
+            if getRegValue(machine, line[0]) >= getRegValue(machine, line[1]):
+                current = marks[line[2]] - 1
+            else:
+                continue
+
+        elif command == 'blt':
+            if getRegValue(machine, line[0]) < getRegValue(machine, line[1]):
+                current = marks[line[2]] - 1
+            else:
+                continue
+
+        else:
+            print('command not found')
+
+        current += 1
+
+        if current == len(instructions):
+            end = True
+
+
+
+
+def getRegValue(machine, name):
+    if machine.registers.get(name) != None:
+        return machine.registers[name]
+    elif machine.registers.get(altRegNames[name]) != None:
+        return machine.registers[altRegNames[name]]
+    else:
+        print('cannot get register value')
+
+
+def setRegValue(machine, name, value):
+    if machine.registers.get(name) != None:
+        machine.registers[name] = value
+    elif machine.registers.get(altRegNames[name]) != None:
+        machine.registers[altRegNames[name]] = value
+    else:
+        print('cannot set register value')
 
 
 def createParser():
@@ -151,9 +275,11 @@ def createParser():
 
 def main():
     machine = simInit()
-    file = parseFile('lisa.s')
-    printRegisters(machine)
+    file = parseFile('temp.s')
     parseDirectives(machine, file)
+    start = startStatus(False, 'main')
+    performInstructions(machine, file.names, file.instructions, start)
+    printRegisters(machine)
     # count = file.names['swap']
     # print(file.instructions[count])
     # parser = createParser()
